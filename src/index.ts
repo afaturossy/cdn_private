4341e1b7c928aedf0f86093600c89ac03b8b1225
ari-fatur <ari.arifatur.fatur@gmail.com>
2022-09-07 14:46:55 +0700

    up cdn

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
 import express from "express";
 import { randomUUID } from "crypto";
 import path from "path";
+import fs from "fs";
 
 import { reqFile } from "./controller";
 import DATAFILE from "./database";
 
 const PORT = process.env.PORT || process.argv[2] || "8000";
 const app = express();
-console.log(process.argv)
+console.log(process.argv);
 
 app.get("/", async (req, res) => {
   const url = req.query.url || "";
 
   // pengecekan datafile exist
-  const r = await DATAFILE.findOne({ where: { url: url } ,order:[["id","DESC"]]});
+  const r = await DATAFILE.findOne({
+    where: { url: url },
+    order: [["id", "DESC"]],
+  });
   if (r) {
-    const filePath = path.resolve(r.path)
-    res.setHeader("Cache-Control",'public, max-age=2592000')
-    res.sendFile(filePath)
+    const filePath = path.resolve(r.path);
+
+    // check file exist
+    const cek = fs.existsSync(filePath);
+
+    if (cek) {
+      res.setHeader("Cache-Control", "public, max-age=2592000");
+      res.sendFile(filePath);
+    } else {
+      const filePath = `public/${randomUUID()}.jpg`;
+      reqFile(req, res, filePath);
+    }
   } else {
     const filePath = `public/${randomUUID()}.jpg`;
     reqFile(req, res, filePath);
